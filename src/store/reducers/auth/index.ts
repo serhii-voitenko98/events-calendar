75cec7a7bd63c15eec0e5d8549329a1cbfd10f0a
Serhii Voitenko <serhii.voitenko98@gmail.com>
2021-09-07 01:18:02 +0300

    add auth reducer

---
+++ src/store/reducers/auth/index.ts
@@ -0,0 +1,32 @@
+import { AuthAction, AuthActionsEnum, AuthState } from './types';
+import { IUser } from '../../../models/user';
+
+const initialState: AuthState = {
+  isAuth: false,
+  error: '',
+  isLoading: false,
+  user: {} as IUser,
+};
+
+export const authReducer = (
+  state = initialState,
+  action: AuthAction
+): AuthState => {
+  switch (action.type) {
+    case AuthActionsEnum.SET_AUTH: {
+      return { ...state, isAuth: action.payload, isLoading: false };
+    }
+    case AuthActionsEnum.SET_USER: {
+      return { ...state, user: action.payload };
+    }
+    case AuthActionsEnum.SET_ERROR: {
+      return { ...state, error: action.payload, isLoading: false };
+    }
+    case AuthActionsEnum.SET_IS_LOADING: {
+      return { ...state, isLoading: action.payload };
+    }
+    default: {
+      return state;
+    }
+  }
+};
