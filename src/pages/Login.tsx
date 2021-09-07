926aa127440867e172cd5d371d2c92ca1932d083
Serhii Voitenko <serhii.voitenko98@gmail.com>
2021-09-07 21:01:16 +0300

    wrap login component by AntD Card

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,4 +1,4 @@
-import { Layout, Row } from 'antd'
+import { Card, Layout, Row } from 'antd'
 import React, { FC } from 'react'
 import { LoginForm } from '../components/LoginForm'
 
@@ -6,7 +6,9 @@ export const Login: FC = () => {
   return (
     <Layout>
       <Row justify="center" align="middle" className="h100">
-        <LoginForm></LoginForm>
+        <Card>
+          <LoginForm></LoginForm>
+        </Card>
       </Row>
     </Layout>
   )
