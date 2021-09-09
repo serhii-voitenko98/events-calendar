b91ebf9b2ae115e21d161e423a94f527be8f91b0
Serhii Voitenko <s.voitenko@spd-ukraine.com>
2021-09-09 21:40:13 +0300

    change auth redux action creator

--- src/store/reducers/auth/actionCreators.ts
+++ src/store/reducers/auth/actionCreators.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import { AppDispatch } from '../..';
+import UserService from '../../../api/UserService';
 import { IUser } from '../../../models/user';
 import {
   AuthActionsEnum,
@@ -26,27 +27,35 @@ export const AuthActionCreators = {
     type: AuthActionsEnum.SET_IS_LOADING,
     payload,
   }),
-  login: (username: string, password: string) => async (dispatch: AppDispatch) => {
-    try {
-      dispatch(AuthActionCreators.setIsLoading(true));
-      setTimeout(async () => {
-        const response = await axios.get<IUser[]>('./users.json');
-        const mockUser = response.data.find(user => user.username === username && user.password === password);
-        if (mockUser) {
-          localStorage.setItem('auth', 'true');
-          localStorage.setItem('username', mockUser.username);
-          dispatch(AuthActionCreators.setAuth(true));
-          dispatch(AuthActionCreators.setUser(mockUser));
-        } else {
-          dispatch(AuthActionCreators.setError('User\'s credentials are wrong'));
-        }
-        dispatch(AuthActionCreators.setIsLoading(false));
-      }, 1000);
-    } catch(e) {
-      dispatch(AuthActionCreators.setError('Something has been wrong'))
-    }
-  },
+  login:
+    (username: string, password: string) => async (dispatch: AppDispatch) => {
+      try {
+        dispatch(AuthActionCreators.setIsLoading(true));
+        setTimeout(async () => {
+          const response = await UserService.getUsers();
+          const mockUser = response.data.find(
+            user => user.username === username && user.password === password
+          );
+          if (mockUser) {
+            localStorage.setItem('auth', 'true');
+            localStorage.setItem('username', mockUser.username);
+            dispatch(AuthActionCreators.setUser(mockUser));
+            dispatch(AuthActionCreators.setAuth(true));
+          } else {
+            dispatch(
+              AuthActionCreators.setError("User's credentials are wrong")
+            );
+          }
+          dispatch(AuthActionCreators.setIsLoading(false));
+        }, 1000);
+      } catch (e) {
+        dispatch(AuthActionCreators.setError('Something has been wrong'));
+      }
+    },
   logout: () => async (dispatch: AppDispatch) => {
-
-  }
+    localStorage.removeItem('auth');
+    localStorage.removeItem('username');
+    dispatch(AuthActionCreators.setUser({} as IUser));
+    dispatch(AuthActionCreators.setAuth(false));
+  },
 };
