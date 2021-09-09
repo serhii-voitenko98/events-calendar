09fb9f8a9f7b2e0da398c964a3eaa0c860af0253
Serhii Voitenko <s.voitenko@spd-ukraine.com>
2021-09-09 21:42:52 +0300

    update main App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import { Layout } from 'antd';
-import React, { FC } from 'react';
+import React, { FC, useEffect } from 'react';
 import { AppRouter } from './components/AppRouter';
 import { Navbar } from './components/Navbar';
 import './App.css';
+import { useActions } from './hooks/useActions';
+import { IUser } from './models/user';
 
 export const App: FC = () => {
+  const {setUser, setAuth} = useActions();
+  useEffect(() => {
+    if (!!localStorage.getItem('auth')) {
+      setAuth(true);
+      setUser({username: localStorage.getItem('username') || ''} as IUser)
+    }
+  }, []);
+
   return (
     <div>
       <Layout>
